86a340d83f629ab0b809149060396a5a871cf4d3
Alec Jandorek <ajandorek@gmail.com>
2018-02-14 16:07:07 -0600

    Added passport and google oauth package

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hey: 'We are up and running!' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server up and running on port ${PORT}`);
-});
+app.listen(PORT);
